d2f140bb9820f315300fdc5f38a56e3a4e3adaf0
Muhammad Haris <imharis021@gmail.com>
2025-07-16 13:47:31 +0500

    Add LoadingScreen and NavBar components, initial setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import './App.css'
+import './index.css'
+import { LoadingScreen } from './components/LoadingScreen.jsx'
+import { useState } from 'react'
+import NavBar from './components/NavBar.jsx'
 
 function App() {
+  const [isLoaded, setIsLoaded] = useState(false)
 
   return (
     <>
-      
+      {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)} />}{" "}
+
+      <div className={` min-h-screen transition-opacity duration-700 ${isLoaded ? "opacity-1000" : "opacity-0"} bg-black text-gray-100 `}>
+        <NavBar />
+      </div>
     </>
   )
 }
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function NavBar() {
+  return (
+    <nav className='fixed top-0 z-40 bg-[rgba(10, 10, 10, 8.8)] back '>
+
+    </nav>
+  )
+}
+
+export default NavBar
